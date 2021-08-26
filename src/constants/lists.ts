61d43079a003883cc7f05fdb7aa3fe7ea7a9afaa
laidakaka <88995693+laidakaka@users.noreply.github.com>
2021-08-26 17:08:35 +0800

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,7 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://www.gemini.com/uniswap/manifest.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://gateway.pinata.cloud/ipfs/QmPcocqzwYYKxPEutRx4U6Y8XVKP6z3RTd293qwFBCeoNo/dswap.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  DEFAULT_TOKEN_LIST_URL
+  DEFAULT_TOKEN_LIST_URL,
+  'https://raw.githubusercontent.com/laidakaka/tokenlist/main/dswap.tokenlist.json'
 ]
